8250436b6316db19239e125e18c92cf4c8ca3d53
Poorva Dattaram Sawant <g25.poorva.sawantdattaram@gnkhalsa.edu.in>
2025-08-10 23:04:12 +0530

    Updated backend code

--- server.js
+++ server.js
@@ -17,11 +17,5 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/auth', authRoutes);
 app.use('/api/recipes', recipeRoutes);
 
-const recipeRoutes = require('./routes/recipeRoutes');
-app.use('/api/recipes', recipeRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const authRoutes = require('./routes/authRoutes');
-app.use('/api/auth', authRoutes);
